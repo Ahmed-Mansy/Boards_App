c6db0d310e48e419b8e8cfba3ad96fe8afc23148
Ahmed-Mansy <Ahmedalimansy1@gmail.com>
2025-04-21 21:40:11 +0200

    Implement user authentication features: add login and change password views, update templates, and enhance navigation for authenticated users.

--- accounts/forms.py
+++ accounts/forms.py
@@ -60,6 +60,8 @@ class SignUpForm(UserCreationForm):
         label="password Confirm"
     )
 
+    
+    
     class Meta:
         model = User
         fields = ['username', 'email','first_name','last_name', 'password1', 'password2',]
